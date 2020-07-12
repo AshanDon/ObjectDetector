c74a75007cb84ec1a94716e5f79497d701c85f5c
Ashan <ashan.anuruddika@icloud.com>
2020-07-12 19:14:46 +0300

    Initial Commit

---
+++ ObjectDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectDetector
+//
+//  Created by Ashan Anuruddika on 7/12/20.
+//  Copyright © 2020 Ashan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
